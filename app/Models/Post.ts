59b357c78c78c5e2ef3d4f401e5aa7c847f2e3c5
Felipe Borges <lipex360@gmail.com>
2021-11-29 22:39:16 -0300

    Adonis CRUD finished

---
+++ app/Models/Post.ts
@@ -0,0 +1,25 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column, beforeCreate } from '@ioc:Adonis/Lucid/Orm'
+import { v4 as uuid } from 'uuid'
+
+export default class Post extends BaseModel {
+  @column({ isPrimary: true })
+  public id: string
+
+  @column()
+  public title: string
+
+  @column()
+  public content: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @beforeCreate()
+  public static insertId(post: Post) {
+    post.id = uuid()
+  }
+}
